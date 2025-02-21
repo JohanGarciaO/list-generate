62512baa72c17dbe4815b1c529411a5b0539ad83
Johan Garcia <johangarciadeoliveira123@outlook.com.br>
2025-02-20 21:38:40 -0300

    finished struct question template

--- src/pdf/list-generate.php
+++ src/pdf/list-generate.php
@@ -5,7 +5,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 use Dotenv\Dotenv;
 use Mpdf\Mpdf;
 
-function size($template_buffer){
+function size($template_buffer)
+{
     $size_elements = new Mpdf([
         'margin_left' => 10,    // Margem esquerda
         'margin_right' => 10,   // Margem direita
@@ -14,7 +15,7 @@ function size($template_buffer){
         'margin_header' => 0,  // Margem do cabeçalho
         'margin_footer' => 0   // Margem do rodapé
     ]);
-    
+
     // Adiciona o cabeçalho em todas as páginas
     $size_elements->SetHTMLHeader('
         <div class="header-container">
@@ -28,7 +29,7 @@ function size($template_buffer){
             </table>
         </div>
     ');
-    
+
     // Adiciona o cabeçalho em todas as páginas
     $size_elements->SetHTMLFooter('
         <div class="footer-container">
@@ -40,10 +41,10 @@ function size($template_buffer){
             </table>
         </div>
     ');
-    
+
     $size_elements->WriteHTML($template_buffer);
-    
-    $last_position = $size_elements->y;    
+
+    $last_position = $size_elements->y;
     return $last_position;
 }
 
@@ -91,7 +92,7 @@ $mpdf->SetHTMLFooter('
     </div>
 ');
 
-$src_style = $_SERVER['DOCUMENT_ROOT'] . "/src/templates/template-style.css" ;
+$src_style = $_SERVER['DOCUMENT_ROOT'] . "/src/templates/template-style.css";
 
 $template = '
 <!DOCTYPE html>
@@ -99,7 +100,7 @@ $template = '
 
 <head>
     <title>Lista Tal</title>
-    <link rel="stylesheet" type="text/css" href="'.$src_style.'">
+    <link rel="stylesheet" type="text/css" href="' . $src_style . '">
 </head>
 
 <body>
@@ -130,46 +131,121 @@ $column = 'left';
 $last_size = 0;
 
 // Objective Question
-function objective_question($number_question, $statement, $json_alternatives){
+function objective_question($number_question, $statement, $json_alternatives)
+{
     $alternatives = json_decode($json_alternatives, true);
 
     if ($alternatives === null) {
         die("Erro ao decodificar JSON: " . json_last_error_msg());
     }
 
-    return '
+    $question = '
     <div class="question">
-        <p><b>' . $number_question . '</b> - ' . $statement . '</p>
-        <div class="options">
-            <span>a) ' . $alternatives['a'] . '</span><br>
-            <span>b) ' . $alternatives['b'] . '</span><br>
-            <span>c) ' . $alternatives['c'] . '</span><br>
-            <span>d) ' . $alternatives['d'] . '</span>
+        <p><b>' . $number_question . '</b> - ' . $statement . '</p>';
+
+    $question .= '<div class="options">';
+    foreach ($alternatives as $key => $value) {
+        $question .= "<span>$key) $value</span><br>";
+    }
+    $question .= '
         </div>
-    </div>
-    ';
+    </div>';
+
+    return $question;
 }
 
-// Objective Gap Question 
-function gap_question($number_question, $statement, $gap_context, $json_alternatives){
+// Gap Question 
+function gap_question($number_question, $statement, $gap_context, $json_alternatives)
+{
     $alternatives = json_decode($json_alternatives, true);
 
     if ($alternatives === null) {
         die("Erro ao decodificar JSON: " . json_last_error_msg());
     }
 
-    return '
+    $question = '
     <div class="question">
         <p><b>' . $number_question . '</b> - ' . $statement . '</p>
-        <p>' . $gap_context . '</p>
-        <div class="options">
-            <span>a) ' . $alternatives['a'] . '</span><br>
-            <span>b) ' . $alternatives['b'] . '</span><br>
-            <span>c) ' . $alternatives['c'] . '</span><br>
-            <span>d) ' . $alternatives['d'] . '</span>
+
+        <p>' . $gap_context . '</p>';
+
+    $question .= '<div class="options">';
+    foreach ($alternatives as $key => $value) {
+        $question .= "<span>$key) $value</span><br>";
+    }
+    $question .= '
         </div>
-    </div>
-    ';
+    </div>';
+
+    return $question;
+}
+
+// Assertive Question 
+function asssertive_question($number_question, $statement, $json_assertions, $json_alternatives)
+{
+    $alternatives = json_decode($json_alternatives, true);
+    $assertions = json_decode($json_assertions, true);
+
+    if ($alternatives === null || $assertions === null) {
+        die("Erro ao decodificar JSON: " . json_last_error_msg());
+    }
+
+    $question = '
+    <div class="question">
+        <p><b>' . $number_question . '</b> - ' . $statement . '</p>';
+
+    $question .= '<p>';
+    foreach ($assertions as $key => $value) {
+        $question .= "<span>$key. $value</span><br>";
+    }
+    $question .= '</p>';
+
+    $question .= '<div class="options">';
+    foreach ($alternatives as $key => $value) {
+        $question .= "<span>$key) $value</span><br>";
+    }
+    $question .= '
+        </div>
+    </div>';
+
+    return $question;
+}
+
+// Correlate Question 
+function correlate_question($number_question, $statement, $json_correlations, $json_alternatives)
+{
+    $alternatives = json_decode($json_alternatives, true);
+    $correlations = json_decode($json_correlations, true);
+
+    if ($alternatives === null || $correlations === null) {
+        die("Erro ao decodificar JSON: " . json_last_error_msg());
+    }
+
+    $question = '
+    <div class="question">
+        <p><b>' . $number_question . '</b> - ' . $statement . '</p>';
+
+    $question .= '<p>';
+    foreach ($correlations['assertive'] as $key => $value) {
+        $question .= "<span>$key. $value</span><br>";
+    }
+    $question .= '</p>';
+
+    $question .= '<p>';
+    foreach ($correlations['related'] as $value) {
+        $question .= "<span>( ) $value</span><br>";
+    }
+    $question .= '</p>';
+
+    $question .= '<div class="options">';
+    foreach ($alternatives as $key => $value) {
+        $question .= "<span>$key) $value</span><br>";
+    }
+    $question .= '
+        </div>
+    </div>';
+
+    return $question;
 }
 
 $objective_statement = "Este é um texto aleatório para testar a estrutura de uma questão, ver aí se deu certo e me confirma:";
@@ -192,21 +268,70 @@ $gap_json_alternatives = '{
 "d": "Água - gelada."
 }';
 
-for($i=1;$i<=4;$i++){    
+$assertive_statement = "Complete as lacunas abaixo e selecione a alternativa correta:";
 
-    if($i%2 == 0){
-        $item = gap_question($i,$gap_statement,$gap_context,$gap_json_alternatives);
-    }else{
-        $item = objective_question($i,$objective_statement,$objective_json_alternatives);
+$assertions = '{
+    "I": "Essa é a assertiva 1.", 
+    "II": "Essa é a assertiva 2.", 
+    "III": "Essa é a assertiva 3.",
+    "IV": "Essa é a assertiva 4."
+    }';
+
+$assertive_json_alternatives = '{
+"a": "I, II e IV.", 
+"b": "Apenas a I.", 
+"c": "I e IV.",
+"d": "Apenas a III."
+}';
+
+$correlate_statement = "Complete as lacunas abaixo e selecione a alternativa correta:";
+
+$correlations = '{
+    "assertive": {
+        "1": "Planeta mais próximo do Sol",
+        "2": "Maior planeta do Sistema Solar",
+        "3": "Único planeta com vida conhecida",
+        "4": "Planeta conhecido como \'Planeta Vermelho\'"
+    },
+    "related": {
+        "1": "Júpiter",
+        "2": "Terra",
+        "3": "Marte",
+        "4": "Mercúrio"
+    }
+}';
+
+$correlate_json_alternatives = '{
+"a": "1-2-3-4.", 
+"b": "1-3-2-4.", 
+"c": "2-1-4-3.",
+"d": "2-1-3-4."
+}';
+
+for ($i = 1; $i <= 10; $i++) {
+
+    if ($i % 2 == 0) {
+        if ($i % 4 == 0) {
+            $item = correlate_question($i, $correlate_statement, $correlations, $correlate_json_alternatives);
+        } else {
+            $item = gap_question($i, $gap_statement, $gap_context, $gap_json_alternatives);
+        }
+    } else {
+        if ($i % 3 == 0) {
+            $item = asssertive_question($i, $assertive_statement, $assertions, $assertive_json_alternatives);
+        } else {
+            $item = objective_question($i, $objective_statement, $objective_json_alternatives);
+        }
     }
 
-    // Se o novo tamanho for menor do que o tamanho antigo é porque houve quebra de página, então joga o item pra próxima coluna
-    if(size($template_buffer . $item) < $last_size){
-        if($column == 'left'){
+    // Se o novo tamanho for menor do que o tamanho antigo é porque haverá quebra de página, então joga o item pra próxima coluna
+    $post_size = size($template_buffer . $item);
+    if ($post_size < $last_size) {
+        if ($column == 'left') {
             $column = 'right';
             $template .= '</div>';
             $template .= '<div class="right-column">';
-        }else{
+        } else {
             $column = 'left';
             $template .= '</div>';
             $template .= '<div class="left-column">';
@@ -217,7 +342,7 @@ for($i=1;$i<=4;$i++){
 
     $template .= $item;
     $template_buffer .= $item;
-    $last_size = size($template_buffer);
+    $last_size = $post_size;
 }
 
 $template .= '</div>';
